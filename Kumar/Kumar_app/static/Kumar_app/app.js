931bf89b5226518eaeefd4f070838c03b7e4d978
AmenoAmena <ardacavusoglu666@gmail.com>
2023-12-23 09:55:20 +0300

    Logic implemented

---
+++ Kumar/Kumar_app/static/Kumar_app/app.js
@@ -0,0 +1,49 @@
+const buttonRed = document.querySelector("#red");
+const buttonGreen = document.querySelector("#green");
+const buttonBlue = document.querySelector("#blue");
+
+let counterWin = 0;
+let counterLose = 0;
+
+
+let win = "win";
+let lose = "lose";
+
+
+const body = document.querySelector("body");
+
+const color =  ["red","blue","green"];
+
+body.style.backgroundColor = "white";
+
+
+buttonRed.addEventListener('click', () => changeColor("red"));
+buttonGreen.addEventListener('click',() => changeColor("green"));
+buttonBlue.addEventListener('click',() => changeColor("blue"));
+
+function changeColor(buttonColor) {
+    const colorIndex = parseInt(Math.random()*color.length);
+    const newColor = color[colorIndex];
+    body.style.backgroundColor = newColor;
+    if (newColor === buttonColor) {
+        counterWin +=1;
+        document.getElementById("winning_state").innerHTML = "You: " + win;
+    }
+    
+    
+    else {
+        counterLose -=1;
+        document.getElementById("winnig_state").innerHTML = "You: " + lose;
+    }
+    if (counterLose === 0 ){
+        counterLose = 0;
+    }
+   
+
+    
+    document.getElementById("win").innerHTML = "Your winning counter is: " + counterWin;
+    document.getElementById("lose").innerHTML = "Your losing counter is: " + counterLose;
+    
+    
+}
+
